3b054f8d25bfbe5dd8d428843680f67eaaede1c7
Einar Hjorleifsson <einar.hjorleifsson@gmail.com>
2016-11-23 16:58:52 +0000

    more functions, more dataobjects

--- R/data_GETALK.R
+++ R/data_GETALK.R
@@ -13,3 +13,19 @@ NULL
 #' @docType data
 #' @author Einar Hjorleifsson
 NULL
+
+#' @name Std.aggregation
+#' @title XXX
+#' @description XXX
+#' location: /net/hafkaldi/export/u2/reikn/Splus5/SMB/GEOMETRY.NEW
+#' @docType data
+#' @author Einar Hjorleifsson
+NULL
+
+#' @name ralllist
+#' @title XXX
+#' @description XXX
+#' location: /net/hafkaldi/export/u2/reikn/Splus5/SMB/ralllist
+#' @docType data
+#' @author Einar Hjorleifsson
+NULL
